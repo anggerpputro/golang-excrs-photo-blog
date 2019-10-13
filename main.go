627b610cce69d5250b2691a56ce5b6cef6f8a9e7
Angger Priyardhan Putro (Laptop HP) <anggerpputro@gmail.com>
2019-10-13 11:45:38 +0700

    upload file

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"crypto/sha1"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/satori/go.uuid"
 )
@@ -19,17 +24,63 @@ func main() {
 	http.HandleFunc("/", index)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 
-	fmt.Println("Server Listening on port :8080...")
+	p := ":9000"
+	fmt.Printf("Server Listening on port %s...\n", p)
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(p, nil)
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	c := getCookie(w, req)
-	tpl.ExecuteTemplate(w, "index.gohtml", c.Value)
+	sc := getSessionCookie(w, req)
+	fc := getFileCookie(w, req)
+
+	// handle form post
+	if req.Method == http.MethodPost {
+		nf, fh, err := req.FormFile("nf")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+
+		// create sha for filename
+		ext := strings.Split(fh.Filename, ".")[1]
+		hash := sha1.New()
+		io.Copy(hash, nf)
+		fname := fmt.Sprintf("%x.%s", hash.Sum(nil), ext)
+
+		// create new file
+		dir, err := os.Getwd() // get current working directory
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(dir, "public", "pics", fname)
+		f, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer f.Close()
+
+		// copy
+		nf.Seek(0, 0)
+		io.Copy(f, nf)
+
+		// append new file to cookie
+		appendToFileCookie(w, fc, fname)
+	}
+
+	// build data for template
+	data := struct {
+		Session string
+		Files   []string
+	}{
+		Session: sc.Value,
+		Files:   strings.Split(fc.Value, "|"),
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
 }
 
-func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+func getSessionCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
 	c, err := req.Cookie("session")
 	if err != nil {
 		sID, err := uuid.NewV4()
@@ -43,5 +94,27 @@ func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
 		}
 		http.SetCookie(w, c)
 	}
+	return c
+}
+
+func getFileCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("files")
+	if err != nil {
+		c = &http.Cookie{
+			Name:  "files",
+			Value: "empty",
+		}
+		http.SetCookie(w, c)
+	}
+	return c
+}
+
+func appendToFileCookie(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
+	s := c.Value
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
+	}
+	c.Value = s
+	http.SetCookie(w, c)
 	return c
 }
